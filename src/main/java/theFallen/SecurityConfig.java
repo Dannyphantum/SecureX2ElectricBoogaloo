f14dbebc273b3917fd4f7eced6d12ff72a19c096
Daniel Bonaparte <abonaparte3@yahoo.com>
2017-07-06 11:28:51 -0400

    Test of heroku deployment

--- src/main/java/theFallen/SecurityConfig.java
+++ src/main/java/theFallen/SecurityConfig.java
@@ -8,6 +8,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
@@ -32,7 +33,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 				.formLogin().failureUrl("/")
 				.defaultSuccessUrl("/homepage")
 				.loginPage("/login")
-				.permitAll();
+				.permitAll()
+				.and()
+				.logout()
+				.logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+				.logoutSuccessUrl("/");
 	}
 	
 	@Override
